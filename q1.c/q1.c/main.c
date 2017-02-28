f4f7650200d35856d8684ab04c39ea345cf111a0
qinyubo <qybo123@gmail.com>
2017-02-28 11:32:44 -0500

    using xcode

---
+++ q1.c/q1.c/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  q1.c
+//
+//  Created by Yubo Qin on 2/28/17.
+//  Copyright (c) 2017 Yubo Qin. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
